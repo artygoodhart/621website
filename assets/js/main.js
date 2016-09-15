5d49a4a9bc9219d6ff1d00322373dd3829bd082a
Arthur Goodhart <arthurgoodhart@Arthurs-MacBook-Pro.local>
2016-09-15 13:25:33 +0100

    different nav for mobile and normal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
     $('#fullpage').fullpage({
       anchors: ['land', 'whoarewe', 'ourteam', 'pioneeringstudents','section5', '621diploma', 'ourcourses', 'pioneeringteaching', 'realworldlearning', 'makeadifference', 'contactus'  ],
@@ -7,6 +9,23 @@ $(document).ready(function() {
 
 });
 
+
+function mobileDropFunction() {
+  document.getElementById("mobile-dropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.fa-bars')) {
+
+    var dropdowns = document.getElementsByClassName("mobile-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 
 function dropFunction() {
@@ -25,31 +44,7 @@ window.onclick = function(event) {
     }
   }
 }
-//
-// $(document).ready(function(){
-//   // Add smooth scrolling to all links
-//   $("a").on('click', function(event) {
-//
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== "") {
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
-//
-//       // Store hash
-//       var hash = this.hash;
-//
-//       // Using jQuery's animate() method to add smooth page scroll
-//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//       }, 800, function(){
-//
-//         // Add hash (#) to URL when done scrolling (default click behavior)
-//         window.location.hash = hash;
-//       });
-//     } // End if
-//   });
-// });
+
 
 // $(window).scroll(function() {
 //   if (Array.prototype.some.call($('.button-change'), function(element) {
