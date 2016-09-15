b3a76bd10758defb19fadac607767ef0595a4159
Arthur Goodhart <arthurgoodhart@Arthurs-MacBook-Pro.local>
2016-09-15 10:28:55 +0100

    working nav bar nearly fully responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
+      anchors: ['land', 'whoarewe', 'ourteam', 'pioneeringstudents','section5', '621diploma', 'ourcourses', 'pioneeringteaching', 'realworldlearning', 'makeadifference', 'contactus'  ],
       responsiveWidth: 992
     }
     );
 
 });
+
+
 
 function dropFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -22,31 +25,31 @@ window.onclick = function(event) {
     }
   }
 }
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+//
+// $(document).ready(function(){
+//   // Add smooth scrolling to all links
+//   $("a").on('click', function(event) {
+//
+//     // Make sure this.hash has a value before overriding default behavior
+//     if (this.hash !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+//
+//       // Store hash
+//       var hash = this.hash;
+//
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
+//
+//         // Add hash (#) to URL when done scrolling (default click behavior)
+//         window.location.hash = hash;
+//       });
+//     } // End if
+//   });
+// });
 
 // $(window).scroll(function() {
 //   if (Array.prototype.some.call($('.button-change'), function(element) {
